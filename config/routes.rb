65eed38c790be872c938d22b16d50ee007ee175d
Jorge Rubio C <jorgerc85@gmail.com>
2015-01-12 16:52:38 -0500

    Port tests resource

--- config/routes.rb
+++ config/routes.rb
@@ -16,6 +16,7 @@ Rails.application.routes.draw do
     resources :patients
     resources :therapists
     resources :evaluations
+    resources :tests
 
   # Example resource route with options:
   #   resources :products do
