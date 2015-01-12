97cba1975b3c38c54da2dac63dc6f1c044f75657
Jorge Rubio C <jorgerc85@gmail.com>
2015-01-12 18:51:52 -0500

    Implement /login route

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -6,4 +6,13 @@ class WelcomeController < ApplicationController
 
   end
 
+  def login
+    therapist = Therapist.find_by(email: params[:therapist][:email]).try(:authenticate, params[:therapist][:password])
+    if therapist
+      session[:therapist_id] = therapist.id
+    else
+      set_error("Email not found or password incorrect.")
+    end
+    redirect("/")
+  end
 end
