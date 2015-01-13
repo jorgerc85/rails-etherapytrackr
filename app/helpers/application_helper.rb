ba6af1c835d9d6a23202d3bbb0e432f1c8378f55
Jorge Rubio C <jorgerc85@gmail.com>
2015-01-12 19:39:14 -0500

    Extract methods to manage the view to application_helper.rb

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,21 @@
 module ApplicationHelper
+
+  def display_error
+    error = session[:error]
+    session[:error] = nil
+    error
+  end
+
+  def set_error(msg)
+    session[:error] = msg
+  end
+
+  def current_therapist
+    if session[:therapist_id]
+      return Therapist.find(session[:therapist_id])
+    else
+      return nil
+    end
+  end
+
 end
