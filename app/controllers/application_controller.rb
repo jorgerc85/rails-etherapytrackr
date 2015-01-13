e1e40bed58333500d976196bb18453549f1d44c2
Jorge Rubio C <jorgerc85@gmail.com>
2015-01-12 19:19:21 -0500

    Extract error handling methods from helper into ApplicationController

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,16 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+  def display_error
+    error = session[:error]
+    session[:error] = nil
+    error
+  end
+
+  def set_error(msg)
+    session[:error] = msg
+  end
+
   def current_therapist
     if session[:therapist_id]
       return Therapist.find(session[:therapist_id])
--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,4 @@
 class WelcomeController < ApplicationController
-  helper ErrorHandlingHelper
 
   def index
 
