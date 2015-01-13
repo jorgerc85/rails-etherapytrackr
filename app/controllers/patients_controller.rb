cf3697701d8e45de4096cb02144874fd55ec8647
Jorge Rubio C <jorgerc85@gmail.com>
2015-01-12 20:51:40 -0500

    Implement Patient creation

--- app/controllers/patients_controller.rb
+++ app/controllers/patients_controller.rb
@@ -1,21 +1,34 @@
 class PatientsController < ApplicationController
-  def index
 
+  def index
   end
 
   def create
-
+    therapist = Therapist.find(session[:therapist_id])
+    patient = therapist.patients.new(patients_params)
+    if patient.save
+      redirect_to "/"
+    else
+      set_error("Failed to register patient.")
+      redirect_to "/"
+    end
   end
 
   def new
-
   end
+
+  # def show
+  # end
 
   def edit
 
   end
 
   def update
-
   end
+
+  def patients_params
+     params.require(:patient).permit(:first_name, :last_name, :birthday, :gender, :height, :weight, :email)
+  end
+
 end
