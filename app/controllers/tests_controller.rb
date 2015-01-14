68b17013f50946defe04945de99fe773d7d7f0f5
Jorge Rubio <jorgerc85@gmail.com>
2015-01-14 00:38:11 -0500

    Progress implementing create tests feature

--- app/controllers/tests_controller.rb
+++ app/controllers/tests_controller.rb
@@ -1,7 +1,21 @@
 class TestsController < ApplicationController
 
-  def create
+  def new
+    @components = Component.all
+  end
 
+  def create
+    test = Test.create(params[:test])
+    components = params[:components]
+    components.each do |comp|
+      tests.components << Component.find_or_create_by(name: comp)
+    end
+  end
+
+  private
+
+  def tests_params
+    params.require(:test).permit(:name, :description)
   end
 
 end
