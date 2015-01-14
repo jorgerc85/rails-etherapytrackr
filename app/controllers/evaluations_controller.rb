200d4aac42b56862a66b367414a49c2da1c249df
Jorge Rubio <jorgerc85@gmail.com>
2015-01-14 00:37:46 -0500

    Progress implementing create evaluations feature

--- app/controllers/evaluations_controller.rb
+++ app/controllers/evaluations_controller.rb
@@ -1,7 +1,14 @@
 class EvaluationsController < ApplicationController
 
-  def create
+  def new
+    @patients = Patient.all
+    @tests = Test.all
+  end
 
+  def create
+    patient = Patient.find_by_email(params[:evaluation][:patient])
+    test = Test.find_by_name(params[:evaluation][:test])
+    patient.evaluation.create(test: test)
   end
 
   def show
